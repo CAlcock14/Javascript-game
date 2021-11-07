5bc000bc7fc28a6be68fe5bf7b4d8ecee49f2bf9
Chris Alcock <chris14@talktalk.net>
2021-11-07 17:11:16 +0000

    Finishing touches

--- newgame/script.js
+++ newgame/script.js
@@ -1,239 +1,127 @@
-const KEY_RIGHT = 39;
-const KEY_LEFT = 37;
-const KEY_SPACE = 32;
+document.addEventListener('DOMContentLoaded', () => {
+  const squares = document.querySelectorAll('.grid div')
+  const resultDisplay = document.querySelector('.results')
+  let width = 15 
+  let currentShooterIndex = 202 
+  let currentInvaderIndex = 2
+  let alienDestroyed = []
+  let results = 0
+  let direction = 1
+  let invaderId
 
-const GAME_WIDTH = 800;
-const GAME_HEIGHT = 600;
+  //constructing the alien invaders
+  //defining which grid squares they are
+  const alienInvaders = [
+    0,1,2,3,4,5,6,7,8,9,
+    15,16,17,18,19,20,21,22,23,24,
+    30,31,32,33,34,35,36,37,38,39
+  ]
 
-const STATE = {
-  x_pos : 0,
-  y_pos : 0,
-  move_right: false,
-  move_left: false,
-  shoot: false,
-  lasers: [],
-  enemyLasers: [],
-  enemies : [],
-  jet_width: 50,
-  enemy_width: 50,
-  cooldown : 0,
-  number_of_enemies: 16,
-  enemy_cooldown : 0,
-  gameOver: false
-}
+  //drawing the invaders
+  alienInvaders.forEach(invader => squares[currentInvaderIndex + invader].classList.add('invader'))
 
+  //drawing the x-wing
+  squares[currentShooterIndex].classList.add('shooter')
 
-// General purpose functions
-function setPosition($element, x, y) {
-  $element.style.transform = `translate(${x}px, ${y}px)`;
-}
-
-function setSize($element, width) {
-  $element.style.width = `${width}px`;
-  $element.style.height = "auto";
-}
-
-function bound(x){
-  if (x >= GAME_WIDTH-STATE.jet_width){
-    STATE.x_pos = GAME_WIDTH-STATE.jet_width;
-    return GAME_WIDTH-STATE.jet_width
-  } if (x <= 0){
-    STATE.x_pos = 0;
-    return 0
-  } else {
-    return x;
-  }
-}
-
-function collideRect(rect1, rect2){
-  return!(rect2.left > rect1.right || 
-    rect2.right < rect1.left || 
-    rect2.top > rect1.bottom || 
-    rect2.bottom < rect1.top);
-}
-
-// Enemy 
-function createEnemy($container, x, y){
-  const $enemy = document.createElement("img");
-  $enemy.src = "img/ELxFZ.png";
-  $enemy.className = "enemy";
-  $container.appendChild($enemy);
-  const enemy_cooldown = Math.floor(Math.random()*100);
-  const enemy = {x, y, $enemy, enemy_cooldown}
-  STATE.enemies.push(enemy);
-  setSize($enemy, STATE.enemy_width);
-  setPosition($enemy, x, y)
-}
-
-function updateEnemies($container){
-  const dx = Math.sin(Date.now()/1000)*40;
-  const dy = Math.cos(Date.now()/1000)*30;
-  const enemies = STATE.enemies;
-  for (let i = 0; i < enemies.length; i++){
-    const enemy = enemies[i];
-    var a = enemy.x + dx;
-    var b = enemy.y + dy;
-    setPosition(enemy.$enemy, a, b);
-    enemy.cooldown = Math.random(0,100);
-    if (enemy.enemy_cooldown == 0){
-      createEnemyLaser($container, a, b);
-      enemy.enemy_cooldown = Math.floor(Math.random()*50)+100 ;
+  //moving the x-wing horizontally 
+  function moveShooter(e) {
+    squares[currentShooterIndex].classList.remove('shooter')
+    switch(e.keyCode) {
+      case 37:
+        if(currentShooterIndex % width !== 0) currentShooterIndex -=1
+        break
+      case 39:
+        if(currentShooterIndex % width < width -1) currentShooterIndex +=1
+        break
     }
-    enemy.enemy_cooldown -= 0.5;
+    squares[currentShooterIndex].classList.add('shooter')
   }
-}
+  document.addEventListener('keydown', moveShooter)
 
-// Player
-function createPlayer($container) {
-  STATE.x_pos = GAME_WIDTH / 2;
-  STATE.y_pos = GAME_HEIGHT - 50;
-  const $player = document.createElement("img");
-  $player.src = "img/F22 jet-258913.png";
-  $player.className = "player";
-  $container.appendChild($player);
-  setPosition($player, STATE.x_pos, STATE.y_pos);
-  setSize($player, STATE.jet_width);
-}
+  //moving the enemy coming down the screen
+  function moveInvaders() {
+    const leftEdge = alienInvaders[0] % width === 0
+    const rightEdge = alienInvaders[alienInvaders.length -1] % width === width -1
 
-function updatePlayer(){
-  if(STATE.move_left){
-    STATE.x_pos -= 3;
-  } if(STATE.move_right){
-    STATE.x_pos += 3;
-  } if(STATE.shoot && STATE.cooldown == 0){
-    createLaser($container, STATE.x_pos - STATE.jet_width/2, STATE.y_pos);
-    STATE.cooldown = 10;
-  }
-  const $player = document.querySelector(".player");
-  setPosition($player, bound(STATE.x_pos), STATE.y_pos-10);
-  if(STATE.cooldown > 0){
-    STATE.cooldown -= 0.5;
-  }
-}
-
-// Player Laser
-function createLaser($container, x, y){
-  const $laser = document.createElement("img");
-  $laser.src = "img/laser.png";
-  $laser.className = "laser";
-  $container.appendChild($laser);
-  const laser = {x, y, $laser};
-  STATE.lasers.push(laser);
-  setPosition($laser, x, y);
-}
-
-function updateLaser($container){
-  const lasers = STATE.lasers;
-  for(let i = 0; i < lasers.length; i++){
-    const laser = lasers[i];
-    laser.y -= 2;
-    if (laser.y < 0){
-      deleteLaser(lasers, laser, laser.$laser);
+    if((leftEdge && direction === -1) || (rightEdge && direction === 1)){
+      direction = width
+      } else if (direction === width){
+        if (leftEdge) direction = 1
+        else direction = -1
+        }
+    for (let i = 0; i <= alienInvaders.length -1; i++){
+      squares[alienInvaders[i]].classList.remove('invader')
     }
-    setPosition(laser.$laser, laser.x, laser.y);
-    const laser_rectangle = laser.$laser.getBoundingClientRect();
-    const enemies = STATE.enemies;
-    for(let j = 0; j < enemies.length; j++){
-      const enemy = enemies[j];
-      const enemy_rectangle = enemy.$enemy.getBoundingClientRect();
-      if(collideRect(enemy_rectangle, laser_rectangle)){
-        deleteLaser(lasers, laser, laser.$laser);
-        const index = enemies.indexOf(enemy);
-        enemies.splice(index,1);
-        $container.removeChild(enemy.$enemy);
+    for (let i = 0; i <= alienInvaders.length -1; i++){
+      alienInvaders[i] += direction
+    }
+    for (let i = 0; i <= alienInvaders.length -1; i++) {
+      if (!alienDestroyed.includes(i)){
+        squares[alienInvaders[i]].classList.add('invader')
+      }
+  }
+
+    //game is over when...the enemy crashes / meets the xwing
+    if (squares[currentShooterIndex].classList.contains('invader', 'shooter')) {
+      resultDisplay.textContent = 'GAME OVER MAN!!'
+      squares[currentShooterIndex].classList.add('boom')
+      clearInterval(invaderId)
+    }
+
+    //game is also over when the enemy reach the bottom of the screen
+    for (let i = 0; i <= alienInvaders.length -1; i++) {
+      if(alienInvaders[i] > (squares.length - (width-1))) {
+        resultDisplay.textContent = 'GAME OVER MAN!!'
+        clearInterval(invaderId)
       }
     }
-  }
-}
 
-// Enemy Laser
-function createEnemyLaser($container, x, y){
-  const $enemyLaser = document.createElement("img");
-  $enemyLaser.src = "img/enemyLaser.png";
-  $enemyLaser.className = "enemyLaser";
-  $container.appendChild($enemyLaser);
-  const enemyLaser = {x, y, $enemyLaser};
-  STATE.enemyLasers.push(enemyLaser);
-  setPosition($enemyLaser, x, y);
-}
-
-function updateEnemyLaser($container){
-  const enemyLasers = STATE.enemyLasers;
-  for(let i = 0; i < enemyLasers.length; i++){
-    const enemyLaser = enemyLasers[i];
-    enemyLaser.y += 2;
-    if (enemyLaser.y > GAME_HEIGHT-30){
-      deleteLaser(enemyLasers, enemyLaser, enemyLaser.$enemyLaser);
+    //When all enemies are destroyed
+    if(alienDestroyed.length === alienInvaders.length) {
+      resultDisplay.textContent = 'YOU WIN!!'
+      clearInterval(invaderId)
     }
-    const enemyLaser_rectangle = enemyLaser.$enemyLaser.getBoundingClientRect();
-    const spaceship_rectangle = document.querySelector(".player").getBoundingClientRect();
-    if(collideRect(spaceship_rectangle, enemyLaser_rectangle)){
-      STATE.gameOver = true;
+  }
+  //invoking the above every 500 milisecs
+  invaderId = setInterval(moveInvaders, 500)    
+
+
+  //to enable the xwing to shoot lasers
+  function shoot(e) {
+    let laserId
+    let currentLaserIndex = currentShooterIndex
+  //laser now moves towards the enemy
+  function moveLaser() {
+    squares[currentLaserIndex].classList.remove('laser')
+    currentLaserIndex -= width
+    squares[currentLaserIndex].classList.add('laser')
+
+    if(squares[currentLaserIndex].classList.contains('invader')) {
+      squares[currentLaserIndex].classList.remove('laser')
+      squares[currentLaserIndex].classList.remove('invader')
+      squares[currentLaserIndex].classList.add('boom')
+
+      setTimeout(()=> squares[currentLaserIndex].classList.remove('boom'), 300)
+      clearInterval(laserId)
+
+      // const alienDestroyed = alienInvaders.indexOf(currentLaserIndex)
+      alienDestroyed.push(alienInvaders.indexOf(currentLaserIndex))
+      results++
+      resultDisplay.textContent = results
     }
-    setPosition(enemyLaser.$enemyLaser, enemyLaser.x + STATE.enemy_width/2, enemyLaser.y+15);
+
+    if(currentLaserIndex < width) {
+      clearInterval(laserId)
+      setTimeout(() => squares[currentLaserIndex].classList.remove('laser'), 100)
+    }
   }
-}
 
-// Delete Laser
-function deleteLaser(lasers, laser, $laser){
-  const index = lasers.indexOf(laser);
-  lasers.splice(index,1);
-  $container.removeChild($laser);
-}
-
-// Key Presses
-function KeyPress(event) {
-  if (event.keyCode === KEY_RIGHT) {
-    STATE.move_right = true;
-  } else if (event.keyCode === KEY_LEFT) {
-    STATE.move_left = true;
-  } else if (event.keyCode === KEY_SPACE) {
-    STATE.shoot = true;
+    switch(e.keyCode) {
+      case 32:
+        laserId = setInterval(moveLaser, 100)
+        break    
+    }
   }
-}
 
-function KeyRelease(event) {
-  if (event.keyCode === KEY_RIGHT) {
-    STATE.move_right = false;
-  } else if (event.keyCode === KEY_LEFT) {
-    STATE.move_left = false;
-  } else if (event.keyCode === KEY_SPACE) {
-    STATE.shoot = false;
-  }
-}
-
-// Main Update Function
-function update(){
-  updatePlayer();
-  updateEnemies($container);
-  updateLaser($container);
-  updateEnemyLaser($container);
-
-  window.requestAnimationFrame(update);
-  
-  if (STATE.gameOver) {
-    document.querySelector(".lose").style.display = "block";
-  } if (STATE.enemies.length == 0) {
-    document.querySelector(".win").style.display = "block";
-  }
-}
-
-function createEnemies($container) {
-  for(var i = 0; i <= STATE.number_of_enemies/2; i++){
-    createEnemy($container, i*80, 100);
-  } for(var i = 0; i <= STATE.number_of_enemies/2; i++){
-    createEnemy($container, i*80, 180);
-  }
-}
-
-// Initialize the Game
-const $container = document.querySelector(".main");
-createPlayer($container);
-createEnemies($container);
-
-// Key Press Event Listener
-window.addEventListener("keydown", KeyPress);
-window.addEventListener("keyup", KeyRelease);
-update();
-
-
+  document.addEventListener('keydown', shoot)
+})
